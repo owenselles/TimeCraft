0ad1dd6327478f1a7cbe43e9d6bc6bbaacc905af
Owen Selles <owensellesyt@gmail.com>
2019-05-22 21:33:47 +0200

    default config

--- src/main/java/com/owenselles/TimeCraft/Main.java
+++ src/main/java/com/owenselles/TimeCraft/Main.java
@@ -3,35 +3,47 @@ package com.owenselles.TimeCraft;
 import com.owenselles.TimeCraft.Commands.ClaimHelp;
 import com.owenselles.TimeCraft.Commands.Discord;
 import com.owenselles.TimeCraft.Commands.Ranks;
+import com.owenselles.TimeCraft.Events.OnPlayerChat;
 import com.owenselles.TimeCraft.Events.OnPlayerJoin;
 import com.owenselles.TimeCraft.Events.OnPlayerLeave;
 import com.owenselles.TimeCraft.Utils.Logger;
-import com.owenselles.TimeCraft.Events.OnPlayerChat;
+import java.util.logging.Level;
 import org.bukkit.Bukkit;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
-import java.util.logging.Level;
+public class Main extends JavaPlugin {
 
-public class Main extends JavaPlugin
-{
-    @Override
-    public void onEnable() {
+  FileConfiguration config = getConfig();
 
-        Bukkit.getPluginCommand("claimhelp").setExecutor(new ClaimHelp());
-        Bukkit.getPluginCommand("discord").setExecutor(new Discord());
-        Bukkit.getPluginCommand("ranks").setExecutor(new Ranks());
-        getServer().getPluginManager().registerEvents(new OnPlayerChat(), this);
-        getServer().getPluginManager().registerEvents(new OnPlayerLeave(), this);
-        getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
+  @Override
+  public void onEnable() {
+    // Default config options
+    config.addDefault("claimhelp", true);
+    config.options().copyDefaults(true);
+    saveConfig();
 
-        Logger.log(Level.INFO, "Success. Plugin Enabled!");
+    //register commands
+    if (config.getBoolean("claimhelp")) {
+      Bukkit.getPluginCommand("claimhelp").setExecutor(new ClaimHelp());
+    } else {
+      Logger.log(Level.INFO, "Claimhelp is disabled in config!!");
     }
+    Bukkit.getPluginCommand("discord").setExecutor(new Discord());
+    Bukkit.getPluginCommand("ranks").setExecutor(new Ranks());
 
-    @Override
-    public void onDisable() {
-        Logger.log(Level.INFO, "Plugin Disabled!");
-    }
+    // register events
+    getServer().getPluginManager().registerEvents(new OnPlayerChat(), this);
+    getServer().getPluginManager().registerEvents(new OnPlayerLeave(), this);
+    getServer().getPluginManager().registerEvents(new OnPlayerJoin(), this);
 
+    Logger.log(Level.INFO, "Success. Plugin Enabled!");
+  }
+
+  @Override
+  public void onDisable() {
+    Logger.log(Level.INFO, "Plugin Disabled!");
+  }
 
 
 }
