79a34380819fd6fd49ea3f838009d4e12aaf64c4
ThatPool <pooldaylumix78@gmail.com>
2019-05-23 22:00:44 +0500

    Fixed afk function
    wont un afk now when move your head

--- src/main/java/com/owenselles/TimeCraft/Commands/Afk.java
+++ src/main/java/com/owenselles/TimeCraft/Commands/Afk.java
@@ -2,6 +2,7 @@ package com.owenselles.TimeCraft.Commands;
 
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -13,22 +14,30 @@ import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 @SuppressWarnings("Duplicates")
 public class Afk implements CommandExecutor, Listener {
 
     private static ArrayList<String> isAFK = new ArrayList<>();
 
+    private Map<Player,String> xyz = new HashMap<>();
+
     @Override
     public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
 
         Player player = (Player) sender;
+        Location loc = player.getLocation();
+        String xyz;
 
         if (isAFK.contains(player.getName())) {
             isAFK.remove(player.getName());
             Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
         } else {
             isAFK.add(player.getName());
+            xyz = Math.floor(loc.getX()) +","+ Math.floor(loc.getY()) + "," + Math.floor(loc.getZ());
+            this.xyz.put(player,xyz);
             Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is now AFK");
         }
 
@@ -38,11 +47,16 @@ public class Afk implements CommandExecutor, Listener {
     @EventHandler
     public void onMove(PlayerMoveEvent event){
         Player player = event.getPlayer();
-
-        if (isAFK.contains(player.getName())){
-            isAFK.remove(player.getName());
-            Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
+        final Location from = event.getFrom();
+        final Location to = event.getTo();
+        if(from.getYaw()==to.getYaw() && to.getPitch()==from.getPitch()) {
+            if (isAFK.contains(player.getName())){
+                isAFK.remove(player.getName());
+                Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
+            }
         }
+
+
 
     }
     @EventHandler
