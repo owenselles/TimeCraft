7ed8471aa59ec26a18a5d07073f66c1e90e08e99
ThatPool <pooldaylumix78@gmail.com>
2019-05-25 20:49:02 +0500

    Add: toggle/edit join/leave message from config
    <player> represents player who joined/left

--- src/main/java/com/owenselles/TimeCraft/Events/OnPlayerJoin.java
+++ src/main/java/com/owenselles/TimeCraft/Events/OnPlayerJoin.java
@@ -1,5 +1,6 @@
 package com.owenselles.TimeCraft.Events;
 
+import com.owenselles.TimeCraft.Main;
 import com.owenselles.TimeCraft.Utils.Color;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
@@ -7,12 +8,22 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 
 public class OnPlayerJoin implements Listener {
+
+    Main main = Main.getPlugin(Main.class);
+
+    private boolean show;
+    private String message;
+
     @EventHandler
     public void onPlayerJoin(PlayerJoinEvent event) {
         if (!event.getPlayer().hasPlayedBefore()) {
             Bukkit.broadcastMessage(Color.add("&7[&a+&7] &7" + event.getPlayer().getName() + " welcome to &6TimeCraft&7!"));
         } else {
-            Bukkit.broadcastMessage(Color.add("&7[&a+&7] &7" + event.getPlayer().getName()));
+            show = Boolean.valueOf(String.valueOf(main.getConfig().get("show-join-message")));
+            if (show) {
+                message = Color.add(String.valueOf(main.getConfig().get("join-message")).replaceAll("<player>",event.getPlayer().getName()));
+                Bukkit.broadcastMessage(message);
+            }
         }
     }
 }
