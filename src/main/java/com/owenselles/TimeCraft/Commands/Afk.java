ac69386c64d7bc24fc1aa02a9e819981bd7e0516
Owen Selles <owensellesyt@gmail.com>
2019-05-25 11:37:55 +0200

    update event

--- src/main/java/com/owenselles/TimeCraft/Commands/Afk.java
+++ src/main/java/com/owenselles/TimeCraft/Commands/Afk.java
@@ -9,6 +9,7 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.event.player.PlayerChatEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
@@ -18,55 +19,55 @@ import java.util.ArrayList;
 @SuppressWarnings("Duplicates")
 public class Afk implements CommandExecutor, Listener {
 
-    private static ArrayList<String> isAFK = new ArrayList<>();
+  private static ArrayList<String> isAFK = new ArrayList<>();
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-        Player player = (Player) sender;
-        if (!player.hasPermission("essentials.afk")) {
-            if (isAFK.contains(player.getName())) {
-                isAFK.remove(player.getName());
-                Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
-            } else {
-                isAFK.add(player.getName());
-                Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is now AFK");
-            }
-        }else{
-            Bukkit.getServer().dispatchCommand(player,"essentials:afk");
-        }
-        return true;
+  @Override
+  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+    Player player = (Player) sender;
+    if (!player.hasPermission("essentials.afk")) {
+      if (isAFK.contains(player.getName())) {
+        isAFK.remove(player.getName());
+        Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
+      } else {
+        isAFK.add(player.getName());
+        Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is now AFK");
+      }
+    } else {
+      Bukkit.getServer().dispatchCommand(player, "essentials:afk");
     }
+    return true;
+  }
 
-    @EventHandler
-    public void onMove(PlayerMoveEvent event){
-        Player player = event.getPlayer();
-        final Location from = event.getFrom();
-        final Location to = event.getTo();
-        if (from.getYaw()==to.getYaw() && to.getPitch()==from.getPitch()) {
-            if (isAFK.contains(player.getName())){
-                player.teleport(from);
-                isAFK.remove(player.getName());
-                Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
-            }
-        }
+  @EventHandler
+  public void onMove(PlayerMoveEvent event) {
+    Player player = event.getPlayer();
+    final Location from = event.getFrom();
+    final Location to = event.getTo();
+    if (from.getYaw() == to.getYaw() && to.getPitch() == from.getPitch()) {
+      if (isAFK.contains(player.getName())) {
+        player.teleport(from);
+        isAFK.remove(player.getName());
+        Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
+      }
     }
+  }
 
-    @EventHandler
-    public void onInteract(PlayerInteractEvent event){
-        Player player = event.getPlayer();
-        if (isAFK.contains(player.getName())){
-            isAFK.remove(player.getName());
-            Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
-        }
+  @EventHandler
+  public void onInteract(PlayerInteractEvent event) {
+    Player player = event.getPlayer();
+    if (isAFK.contains(player.getName())) {
+      isAFK.remove(player.getName());
+      Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
     }
+  }
 
-    @EventHandler
-    public void onChat(PlayerChatEvent event){
-        Player player = event.getPlayer();
-        if (isAFK.contains(player.getName())){
-            isAFK.remove(player.getName());
-            Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
-        }
+  @EventHandler
+  public void onChat(AsyncPlayerChatEvent event) {
+    Player player = event.getPlayer();
+    if (isAFK.contains(player.getName())) {
+      isAFK.remove(player.getName());
+      Bukkit.broadcastMessage(ChatColor.GRAY + player.getName() + " is no longer AFK");
     }
+  }
 
 }
